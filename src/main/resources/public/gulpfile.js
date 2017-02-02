fb97c91ce5eb18c4e8d92097c36fa3102dcdd874
Igor Lychak <k4k7005@gmail.com>
2017-02-02 19:58:06 +0200

    add api proxy to the gulpfile.js

--- src/main/resources/public/gulpfile.js
+++ src/main/resources/public/gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var proxy = require('http-proxy-middleware');
+
+var apiProxy = proxy('/api', {
+    target: 'http://localhost:8080',
+    changeOrigin: false,
+    logLevel: 'debug'
+});
 
 gulp.task('serve', [], function() {
   // .init starts the server
   browserSync.init({
     server: "./",
-    port: 3000
+    port: 3000,
+    middleware: [apiProxy],
   });
 
   gulp.watch(['**/*.html', '**/*.css', '**/*.js'], {
