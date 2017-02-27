778e6bb96d95c03f2b09c987e304d2ff6b853d29
Igor Lychak <k4k7005@gmail.com>
2017-02-27 17:12:23 +0200

    delete unneeded AppErrorController

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,7 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -22,9 +25,16 @@ public class Application {
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("index.html");
                 registry.addViewController("/about").setViewName("about.html");
-                registry.addViewController("/error").setViewName("404.html");
             }
 
+        };
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return container -> {
+            final ErrorPage notFoundPage = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            container.addErrorPages(notFoundPage);
         };
     }
 
