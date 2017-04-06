77438a866cb63ffef9b75d1957332effcaa5522f
Igor Lychak <k4k7005@gmail.com>
2017-04-06 23:50:25 +0300

    replace @Bean's from InpackerApp to AppConfig class

--- src/main/java/inpacker/web/AppConfig.java
+++ src/main/java/inpacker/web/AppConfig.java
@@ -2,8 +2,14 @@ package inpacker.web;
 
 import inpacker.core.*;
 import inpacker.instagram.*;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.File;
 
@@ -11,6 +17,25 @@ import java.io.File;
 public class AppConfig {
 
     private static final File packsDir = new File("/app/tmp/");
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("index.html");
+                registry.addViewController("/about").setViewName("about.html");
+            }
+        };
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return container -> {
+            final ErrorPage notFoundPage = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            container.addErrorPages(notFoundPage);
+        };
+    }
 
     @Bean("ig")
     public DefaultPackService<IgPackConfig, IgPackItem> defaultPackService() {
--- src/main/java/inpacker/web/InpackerApp.java
+++ src/main/java/inpacker/web/InpackerApp.java
@@ -2,40 +2,12 @@ package inpacker.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class InpackerApp {
 
     public static void main(String[] args) {
         SpringApplication.run(InpackerApp.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("index.html");
-                registry.addViewController("/about").setViewName("about.html");
-            }
-
-        };
-    }
-
-    @Bean
-    public EmbeddedServletContainerCustomizer containerCustomizer() {
-        return container -> {
-            final ErrorPage notFoundPage = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-            container.addErrorPages(notFoundPage);
-        };
     }
 
 }
