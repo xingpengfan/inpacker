5dbed4ee165068149e6b5d33052b8b0da8a8c523
Igor Lychak <k4k7005@gmail.com>
2017-04-08 22:43:25 +0300

    replace web configurations from AppConfig to WebConfig

--- src/main/java/inpacker/web/AppConfig.java
+++ src/main/java/inpacker/web/AppConfig.java
@@ -2,14 +2,8 @@ package inpacker.web;
 
 import inpacker.core.*;
 import inpacker.instagram.*;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.File;
 
@@ -17,24 +11,6 @@ import java.io.File;
 public class AppConfig {
 
     private static final File packsDir = new File("/app/tmp/");
-
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("index.html");
-            }
-        };
-    }
-
-    @Bean
-    public EmbeddedServletContainerCustomizer containerCustomizer() {
-        return container -> {
-            final ErrorPage notFoundPage = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-            container.addErrorPages(notFoundPage);
-        };
-    }
 
     @Bean("igPackService")
     public PackService<IgPackConfig, IgPackItem> packService() {
