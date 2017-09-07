ed04bac375228eb4ecb57ca3963a9865dc2da496
Igor <k4k7005@gmail.com>
2017-09-07 05:06:31 +0300

    web: replace all packs related stuff from InstagramController to PackController

---
+++ web/src/main/java/inpacker/web/controller/PackController.java
@@ -0,0 +1,75 @@
+package inpacker.web.controller;
+
+import inpacker.core.Pack;
+import inpacker.core.PackService;
+import inpacker.instagram.IgItemRepository;
+import inpacker.instagram.IgPackConfig;
+import inpacker.instagram.IgPackItem;
+import inpacker.instagram.IgUser;
+import inpacker.web.dto.CreatePackRequest;
+import inpacker.web.dto.PackStatusResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+
+import static inpacker.web.dto.MessageResponse.invalidCreatePackRequestBody;
+import static inpacker.web.dto.MessageResponse.packNotFound;
+import static inpacker.web.dto.MessageResponse.userNotFound;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.http.ResponseEntity.status;
+
+@Controller
+public class PackController {
+
+    private final PackService<IgPackConfig, IgPackItem> packService;
+    private final IgItemRepository igRepo;
+
+    @Autowired
+    public PackController(@Qualifier("igPackService") PackService<IgPackConfig, IgPackItem> packService,
+                               IgItemRepository igRepo) {
+        this.packService = packService;
+        this.igRepo = igRepo;
+    }
+
+    @PostMapping("api/packs")
+    public ResponseEntity<?> createPack(@RequestBody CreatePackRequest req) {
+        if (!isValidRequest(req))
+            return status(UNPROCESSABLE_ENTITY).body(invalidCreatePackRequestBody());
+        final IgUser user = igRepo.getInstagramUser(req.username);
+        if (user == null)
+            return status(NOT_FOUND).body(userNotFound(req.username));
+        final IgPackConfig config = req.toIgPackConfig(user);
+        final Pack pack = packService.createPack(config);
+        return ok(new PackStatusResponse(pack));
+    }
+
+    @GetMapping("api/packs/{packId:.+}/status")
+    public ResponseEntity<?> getPackStatus(@PathVariable("packId") String packId) {
+        final Pack pack = packService.getPack(packId);
+        if (pack == null)
+            return status(NOT_FOUND).body(packNotFound(packId));
+        else
+            return ok(new PackStatusResponse(pack));
+    }
+
+    @GetMapping(value = "packs/{packId:.+}.zip", produces = "application/zip")
+    @ResponseBody
+    public ResponseEntity<FileSystemResource> downloadPack(@PathVariable("packId") String packId) {
+        final File packFile = packService.getPackFile(packId);
+        if (packFile == null)
+            return status(NOT_FOUND).body(null);
+        else
+            return ok(new FileSystemResource(packFile));
+    }
+
+    public static boolean isValidRequest(CreatePackRequest req) {
+        return req.username != null && !req.username.trim().isEmpty() && (req.includeImages || req.includeVideos);
+    }
+}
